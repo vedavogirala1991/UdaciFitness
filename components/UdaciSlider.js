fb8aef8fb2ec583465d0d244d6a4ecce377045c0
veda vogirala <veda.vogirala@gmail.com>
2021-02-09 02:35:30 +0530

    Udacity Fitness App - Adding the Stepper component

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,25 +1,23 @@
 import React,{Component} from 'react'
 import { View, Text, Slider } from 'react-native'
 
-class UdaciSlider extends Component {
-  render () {
-    return (
+const UdaciSlider = ({max, unit, step, value, onChange}) => {
+  return (
+    <View>
+      <Slider
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
       <View>
-        <Slider
-          step={step}
-          value={value}
-          maximumValue={max}
-          minimumValue={0}
-          onValueChange={onChange}
-        />
-        <View>
-          <Text>{value}</Text>
-          <Text>{unit}</Text>
-        </View>
-        
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
       </View>
-    )
-  }
+
+    </View>
+  )
 }
 
 export default UdaciSlider
